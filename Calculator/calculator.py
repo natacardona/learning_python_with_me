d35618e38ba90c758991b39918eab240847b6c80
natcardon <carol.cardona@yaydoo.com>
2024-01-13 18:56:57 -0500

    Add validation to allow only int values

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,6 +1,13 @@
 def sum(num1,num2):
-    return num1+num2
+    # Check if the variable is an integer
+    if isinstance(num1, int) and isinstance(num2,int):
+        return num1+num2
+    else:
+        print("This method not allow strings.")
+    
 
 #Testing sum
 seven = sum(1,6)
+test_string = sum("1", "6")
 print(seven)
+print(test_string)
