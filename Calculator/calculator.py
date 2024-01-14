7ae0e3c0c96a06fa657a8acd0b47ad4398e463a7
natcardon <carol.cardona@yaydoo.com>
2024-01-13 19:47:50 -0500

    Refactoring method

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,14 +1,8 @@
-def sum(num1,num2):
-    # Check if the input values are numbers
-    if (isinstance(num1, int) and isinstance(num2,int)) or (isinstance(num1, float) and isinstance(num2,float)) or (isinstance(num1, int) and isinstance(num2,float)) or (isinstance(num1, float) and isinstance(num2,int)) :
-        return num1+num2
+from numbers import Real
+
+def sum(num1, num2):
+    # Check if both inputs are numbers
+    if isinstance(num1, Real) and isinstance(num2, Real):
+        return num1 + num2
     else:
-        print("This method not allow strings.")
-    
-
-#Test cases
-seven = sum(1,6)
-test_string = sum("1", "6")
-test_float = sum(1720,232)
-test_float_and_number = sum(1, 2.3)
-
+        print("This method only allows numeric values.")
