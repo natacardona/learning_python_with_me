d849afef6fdaba02d216072051963f66b8987a94
natcardon <carol.cardona@yaydoo.com>
2024-01-13 19:26:34 -0500

    Add validation with each type of data

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,15 +1,14 @@
 def sum(num1,num2):
-    # Check if the variable is an integer
-    if (isinstance(num1, int) and isinstance(num2,int)) or (isinstance(num1, float) and isinstance(num2,float)):
+    # Check if the input values are numbers
+    if (isinstance(num1, int) and isinstance(num2,int)) or (isinstance(num1, float) and isinstance(num2,float)) or (isinstance(num1, int) and isinstance(num2,float)) or (isinstance(num1, float) and isinstance(num2,int)) :
         return num1+num2
     else:
         print("This method not allow strings.")
     
 
-#Testing sum
+#Test cases
 seven = sum(1,6)
 test_string = sum("1", "6")
 test_float = sum(1720,232)
-print(seven)
-print(test_string)
-print(test_float)
+test_float_and_number = sum(1, 2.3)
+
