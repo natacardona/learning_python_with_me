0ba0fb83e3c0980946e19631459d56314f926e28
natcardon <carol.cardona@yaydoo.com>
2024-01-13 19:16:21 -0500

    Add least validationn about float numbers

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,6 +1,6 @@
 def sum(num1,num2):
     # Check if the variable is an integer
-    if isinstance(num1, int) and isinstance(num2,int):
+    if (isinstance(num1, int) and isinstance(num2,int)) or (isinstance(num1, float) and isinstance(num2,float)):
         return num1+num2
     else:
         print("This method not allow strings.")
@@ -9,5 +9,7 @@ def sum(num1,num2):
 #Testing sum
 seven = sum(1,6)
 test_string = sum("1", "6")
+test_float = sum(1720,232)
 print(seven)
 print(test_string)
+print(test_float)
