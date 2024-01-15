383b127b95f454f5d30048e40d46f0b49430a12f
natcardon <carol.cardona@yaydoo.com>
2024-01-15 18:05:24 -0500

    Creating a new method substration

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -6,3 +6,10 @@ def sum(num1, num2):
         return num1 + num2
     else:
         print("This method only allows numeric values.")
+
+def subs(num1, num2):
+    # Check if both inputs are numbers
+    if isinstance(num1, Real) and isinstance(num2, Real):
+        return num1 - num2
+    else:
+        print("This method only allows numeric values.")             
