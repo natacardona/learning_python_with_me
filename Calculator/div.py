6613bb7d899d583268180dfea4ffb70a57589a47
Jose Luis Rodriguez Contreras <jose.rodriguezcontreras@externos-cl.cencosud.com>
2024-01-16 19:14:49 -0500

    Adding exception to get division by zero

--- Calculator/div.py
+++ Calculator/div.py
@@ -3,4 +3,7 @@ from numbers import Real
 def div(num1, num2):
     # Check if both inputs are numbers
     if isinstance(num1, Real) and isinstance(num2, Real):
-        return num1 / num2
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            raise Exception("You can't divide any number by zero.") 
